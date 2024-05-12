b73ce7de849caaf480fe6fb49b400edbeebd43ef
zen0-5338 <shahjeet767@gmail.com>
2024-05-13 00:38:18 +0530

    Added cloned files from HelloWorld Activity

---
+++ activity.py
@@ -0,0 +1,65 @@
+# Copyright 2009 Simon Schampijer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""HelloWorld Activity: A case study for developing an activity."""
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+from gettext import gettext as _
+
+from sugar3.activity import activity
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import StopButton
+from sugar3.activity.widgets import ActivityToolbarButton
+
+
+class HelloWorldActivity(activity.Activity):
+    """HelloWorldActivity class as specified in activity.info"""
+
+    def __init__(self, handle):
+        """Set up the HelloWorld activity."""
+        activity.Activity.__init__(self, handle)
+
+        # we do not have collaboration features
+        # make the share option insensitive
+        self.max_participants = 1
+
+        # toolbar with the new toolbar redesign
+        toolbar_box = ToolbarBox()
+
+        activity_button = ActivityToolbarButton(self)
+        toolbar_box.toolbar.insert(activity_button, 0)
+        activity_button.show()
+
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_expand(True)
+        toolbar_box.toolbar.insert(separator, -1)
+        separator.show()
+
+        stop_button = StopButton(self)
+        toolbar_box.toolbar.insert(stop_button, -1)
+        stop_button.show()
+
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show()
+
+        # label with the text, make the string translatable
+        label = Gtk.Label(_("Hello World!"))
+        self.set_canvas(label)
+        label.show()
